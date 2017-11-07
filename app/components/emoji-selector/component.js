9396c8a6f814671c40213d16d4ae5ccdf99d2e49
Jacques Thrasybule <Jacques.Thrasybule@inin.com>
2017-11-07 14:13:36 -0500

    Fixed some styles... center main page and seperate entries

--- app/components/emoji-selector/component.js
+++ app/components/emoji-selector/component.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 import emojiJson from '../../utils/emojiJson';
 
+const {
+  computed,
+} = Ember;
+
 export default Ember.Component.extend({
   classNames: ['emoji-selector'],
-  emojiList: [],
+  emojiList: null,
+
   showPeopleList: false,
   showNatureList: false,
   showObjectsList: false,
@@ -32,8 +37,29 @@ export default Ember.Component.extend({
     }
   },
 
+  peopleList: computed.filterBy('emojiList', 'category', 'people'),
+  natureList: computed.filterBy('emojiList', 'category', 'nature'),
+  objectsList: computed.filterBy('emojiList', 'category', 'objects'),
+  placesList: computed.filterBy('emojiList', 'category', 'places'),
+  symbolsList: computed.filterBy('emojiList', 'category', 'symbols'),
+
+  totalCount: computed('peopleList.length', 'natureList.length', 'objectsList.length', 'placesList.length', 'symbolsList.length', function () {
+    return this.get('peopleList.length') + this.get('natureList.length') + this.get('objectsList.length') + this.get('placesList.length') + this.get('symbolsList.length');
+  }),
+
   init() {
     this._super(...arguments);
     this.set('emojiList', emojiJson);
-  }
+    this.initCategory();
+    console.log('JMT REference to component =>', this);
+  },
+
+  initCategory() {
+    this.get('emojiList').forEach(emojiItem => {
+      if (!Ember.get(emojiItem, 'category')) {
+        Ember.set(emojiItem, 'category', 'null');
+      }
+    });
+  },
+
 });
