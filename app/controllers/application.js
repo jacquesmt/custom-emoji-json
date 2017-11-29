854dd83f28b473810812ce8d2b244d0c517bd1b4
Jacques Thrasybule <Jacques.Thrasybule@inin.com>
2017-11-29 12:31:19 -0500

    Added application controller to access waiting event

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    application: Ember.inject.service()
+    application: Ember.inject.service(),
+
+    actions: {
+        loadEmojiList() {
+          this.set('application.waitTimerOn', true);
+          Ember.run.next(this, function () {
+            this.get('router').transitionTo('main');
+          });
+        }
+    }
 });
